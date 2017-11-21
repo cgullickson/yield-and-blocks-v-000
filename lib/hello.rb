d2acfd411f070c41ad7ea522c8ea34d008a2c4fa
Chris Gullickson <cgullickson5@gmail.com>
2017-11-21 22:08:23 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,8 +7,9 @@ def hello_t(array)
     i = i + 1
     end
     array
-else
-  puts "Hey! No block given!"
+  else
+    puts "Hey! No block given!"
+  end
 end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
